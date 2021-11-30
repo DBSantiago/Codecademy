97af01d98ca39b0c9e1df6e3e5d387ce2fc5433d
DBSantiago <deblasissantiago@gmail.com>
2021-11-30 19:08:03 -0300

    Agregar 12-miniLinter.js a 44-javaScript-syntax

---
+++ FrontEndEngineer/44-javaScript-syntax/12-miniLinter.js
@@ -0,0 +1,40 @@
+let story =
+  'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
+
+let overusedWords = ["really", "very", "basically"];
+
+let unnecessaryWords = ["extremely", "literally", "actually"];
+
+let individualWords = story.split(" ");
+
+let betterWords = individualWords.filter((word) => {
+  return !unnecessaryWords.includes(word);
+});
+
+let timesReallyUsed = 0;
+let timesVeryUsed = 0;
+let timesBasicallyUsed = 0;
+
+for (let word of betterWords) {
+  if (word === "really") {
+    timesReallyUsed += 1;
+  } else if (word === "very") {
+    timesVeryUsed += 1;
+  } else if (word === "basically") {
+    timesBasicallyUsed += 1;
+  }
+}
+
+let sentences = 0;
+individualWords.forEach((word) => {
+  if (word[word.length - 1] === "." || word[word.length - 1] === "!") {
+    sentences += 1;
+  }
+});
+console.log("Total words count: " + individualWords.length);
+console.log(`Total sentences count: ${sentences}`);
+console.log(`Total 'really' used count: ${timesReallyUsed}`);
+console.log(`Total 'very' used count: ${timesVeryUsed}`);
+console.log(`Total 'basically' used count: ${timesBasicallyUsed}`);
+console.log("Improved story: ");
+console.log(betterWords.join(" "));
